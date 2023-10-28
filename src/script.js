045b19aa09aa83acee9ca679a949409e896126f4
Michle99 <sammiyonyx@gmail.com>
2023-10-28 18:27:33 -0400

    Added requirement 2 to script.js

--- src/script.js
+++ src/script.js
@@ -1,2 +1,5 @@
 // Requirement 1: Cache an element using getElementById
 const boardElement = document.getElementById("board");
+const messageElement = document.getElementById("message");
+// Requirement 2: Cache elements using querySelector
+const nameForm = document.querySelector("#name-form");
