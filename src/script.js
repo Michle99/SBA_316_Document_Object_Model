5fbe3c3f3cf97f5b9b1891c163023591ae6b45cc
Michle99 <sammiyonyx@gmail.com>
2023-10-28 18:43:25 -0400

    Added requirement 8 & handleCellClick function to script.js

--- src/script.js
+++ src/script.js
@@ -31,3 +31,60 @@ let gameOver = false;
 for (const cell of boardElement.children) {
   cell.addEventListener("click", handleCellClick);
 }
+
+function handleCellClick(event) {
+  if (gameOver) return;
+
+  const cell = event.target;
+  const index = cell.dataset.index;
+
+  if (gameBoard[index] === "") {
+    // Requirement 8: Modify the HTML content of an element
+    cell.innerHTML = currentPlayer;
+    // Requirement 9: Modify the style of an element using classList
+    // cell.classList.add("disabled");
+    // cell.classList.add("clicked");  // Requirement 10:
+    // gameBoard[index] = currentPlayer;
+
+    // if (checkForWin(currentPlayer)) {
+      // Hightlight the wining cell combination
+      // const winCellTiles = getWinningTiles(currentPlayer);
+      // // Remove winning-cell class from all cells
+      // for (const boardCell of boardElement.children) {
+      //   boardCell.classList.remove("clicked");
+      // }
+      // for (const winIndex of winCellTiles) {
+      //   boardElement.children[winIndex].classList.add("winning-cell");
+      // }
+
+      // // Delay to display the winning cells before showing the alert
+      // setTimeout(() => {
+      //   if (confirm(`Player ${currentPlayer} wins! Play again?`)) {
+      //     // restartGame();
+      //   } else {
+      //     endGame();
+      //   }
+      //   // Requirement 12: Use window.alert
+      //   // window.alert(`Player ${currentPlayer} wins!`);
+      //   // endGame();
+      // }, 2000);
+    // } else if (gameBoard.every((cell) => cell !== "")) {
+    //   // Requirement 12: Use window.alert
+    //   // window.alert("It's a draw!");
+    //   setTimeout(() => {
+    //     if (confirm("It's a draw! Play again?")) {
+    //       restartGame();
+    //     } else {
+    //       endGame();
+    //     }
+    //   }, 500);
+    //   // endGame();
+    // } else {
+    //   currentPlayer = currentPlayer === "X" ? "O" : "X";
+    //   // Requirement 8: Modify the text content of an element
+    //   messageElement.textContent = `Player ${currentPlayer}'s turn`;
+
+    //   setTimeout(computerTurn, 500); // Requirement 12: BOM method
+    // }
+  }
+}
