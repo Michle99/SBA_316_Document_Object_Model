0d1471302d2ad7e90ea58ae4905c0477c7301f6a
Michle99 <sammiyonyx@gmail.com>
2023-10-28 18:34:22 -0400

    Added requirement 3 to script.js

--- src/script.js
+++ src/script.js
@@ -3,3 +3,17 @@ const boardElement = document.getElementById("board");
 const messageElement = document.getElementById("message");
 // Requirement 2: Cache elements using querySelector
 const nameForm = document.querySelector("#name-form");
+// Requirement 3: Use parent-child-sibling relationships
+const formParent = nameForm.parentNode;
+
+//Requirement 7: Create a DocumentFragment for the board
+// const fragment = new DocumentFragment();
+// Requirement 4: Iterate over a collection of elements to accomplish a task
+// for (let i = 0; i < 9; i++) {
+  // Requirement 5: Create an element using createElement
+  // const cell = document.createElement("div");
+  // cell.classList.add("cell");
+  // cell.dataset.index = i;
+  // Requirement 6: Append created div cell to the document fragment
+  // fragment.appendChild(cell);
+// }
