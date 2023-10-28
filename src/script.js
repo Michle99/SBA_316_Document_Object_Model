2fac42374eee50ec067493acfa75da296367c7c7
Michle99 <sammiyonyx@gmail.com>
2023-10-28 18:58:33 -0400

    Added getWinningTiles() function to script.js

--- src/script.js
+++ src/script.js
@@ -107,3 +107,30 @@ function restartGame() {
     messageElement.textContent = "Player's turn";
   }
 }
+
+// Get the winning tiles
+function getWinningTiles(player) {
+  const winPatterns = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (const pattern of winPatterns) {
+    const [a, b, c] = pattern;
+    if (
+      gameBoard[a] === player &&
+      gameBoard[b] === player &&
+      gameBoard[c] === player
+    ) {
+      return pattern; // Return the winning combination
+    }
+  }
+
+  return [];
+}
