4e6c2c9a9c8c46e566504a0ec6f11353909092da
Michle99 <sammiyonyx@gmail.com>
2023-10-28 19:01:01 -0400

    Added checkForWin(player) to script.js

--- src/script.js
+++ src/script.js
@@ -165,3 +165,30 @@ function computerTurn() {
     currentPlayer = currentPlayer === "X" ? "O" : "X";
   }
 }
+
+// checkForWin(player) to check for the winner
+function checkForWin(player) {
+  const winPatterns = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (const pattern of winPatterns) {
+    const [a, b, c] = pattern;
+    if (
+      gameBoard[a] === player &&
+      gameBoard[b] === player &&
+      gameBoard[c] === player
+    ) {
+      return true;
+    }
+  }
+
+  return false;
+}
